91c09e09612abb376f6b7b32f6aad23a05bb1c7d
Håkon Antonsen <hakon.antonsen@sintef.no>
2018-05-04 10:51:11 +0200

    Move all source files to src, start implementing React

---
+++ src/index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './js/App.js';
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ src/js/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router';
+
+export default (props) =>
+    <Router>
+        
+    </Router>;
